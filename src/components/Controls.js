a3eb3ee6f99c8223c0bb998a47e8916efb116ea8
Esteban I. RM <me@exio4.xyz>
2018-07-25 17:52:20 -0300

    add controls [with broken css] + pick rendered tiles from window size

---
+++ src/components/Controls.js
@@ -0,0 +1,55 @@
+// @flow
+
+import React from 'react'
+
+import type { Action } from '../redux/actions'
+
+
+type Props = {
+    move: (number, number) => Action
+}
+
+let style = {
+    'flex': '0 0 auto', 
+    'backgroundColor': '#555',
+    'margin': '5px'
+}
+
+let button = {
+    'position': 'absolute',
+    'top': '0',
+    'left': '0',
+    'width': '5vmin',
+    'height': '5vmin',
+    'fontSize': '4vmin',
+    'textAlign': 'center',
+    'color': '#30f',
+    'backgroundColor': '#fff',
+}
+let up = {...button,
+    'transform': 'translate(5vmin, 0vmin)'
+}
+let down = {...button,
+    'transform': 'translate(5vmin, 10vmin)'
+}
+let left = {...button,
+    'transform': 'translate(0vmin, 5vmin)'
+}
+let right = {...button, 
+    'transform': 'translate(10vmin, 5vmin)'
+}
+
+const Controls = (props: Props) => {
+    let e_up    = () => props.move( 0, -1)
+    let e_down  = () => props.move( 0,  1)
+    let e_left  = () => props.move(-1,  0)
+    let e_right = () => props.move( 1,  0)
+    return (<div style={style}>
+        <div style={up}    onClick={e_up}    onTouchdown={e_up}   >⬆️</div>
+        <div style={down}  onClick={e_down}  onTouchdown={e_down} >⬇️</div>
+        <div style={left}  onClick={e_left}  onTouchdown={e_left} >⬅️</div>
+        <div style={right} onClick={e_right} onTouchdown={e_right}>➡️</div>
+     </div>
+    )
+}
+export default Controls
--- src/components/Map.js
+++ src/components/Map.js
@@ -2,7 +2,9 @@
 
 import React from 'react'
 import ArrowKeysReact from 'arrow-keys-react'
+
 import Tile from './Tile'
+import Controls from './Controls'
 
 import type { GameMap } from '../logic/Game'
 import { getNearby } from '../logic/Game'
@@ -16,49 +18,82 @@ export type MapProps = {
   move: (number, number) => Action
 }
 
-let style = {
-  'position': 'absolute',
-  'top': '50%',
-  'left': '50%',
-  'justify-content': 'center',
-  'align-items': 'center',
-  'height': '640px',
-  'width': '640px',
-  'transform': 'translate(-50%, -50%)',
-  'backgroundColor': '#333'
+type MapState = {
+    blocks: number,
+    win: WindowSize,
 }
 
-export const Map = (props: MapProps) => {
+let flexbox = {
+    'display': 'flex',
+    'justifyContext': 'center',
+}
 
-  let around = getNearby(props.map, 10)
+let style = (blocks: number) => ({
+  'flex': '0 0 auto', 
+  'position': 'absolute',
+  'height': String(32 * (blocks*2)) + 'px',
+  'width': String(32 * (blocks*2)) + 'px',
+  'backgroundColor': '#333'
+})
 
-  ArrowKeysReact.config({
-    left: () => {
-      props.move(-1, 0)
-    },
-    right: () => {
-      props.move(1, 0)
-    },
-    up: () => {
-      props.move(0, -1)
-    },
-    down: () => {
-      props.move(0, 1)
+export class Map extends React.Component<MapProps, MapState> {
+    constructor(props: MapProps) {
+        super(props)
+        this.state = { blocks: 0, win: { height: 0, width: 0 } }
     }
-  });
+    
+    componentDidMount() {
+        this.updateWindowDimensions();
+        window.addEventListener('resize', this.updateWindowDimensions);
+    }
 
-  return (<div  {...ArrowKeysReact.events} tabIndex="1" style={style}>
-    {around.map((row, x) =>  
-      row.map((tile, y) => (
-        <Tile {...tile} pos={[x,y]} key={x * 1024 + y} important={x === 10 && y === 10}>
-          {
-            (x === 10 && y === 10) ? <img className="idle" /> : null
-          }
-        </Tile>
-      ))
-    )}
-  </div>)
+    componentWillUnmount() {
+        window.removeEventListener('resize', this.updateWindowDimensions);
+    }
 
+    updateWindowDimensions = () => {
+        let width = window.innerWidth
+        let height = window.innerHeight
+        let ratio = Math.round(((width > height ? height : width) / 32)/2) - 2
+        
+        this.setState({ blocks: ratio, win: { width: window.innerWidth, height: window.innerHeight }});
+    }
+    render() {
+        let around = getNearby(this.props.map, this.state.blocks)
+        
+        ArrowKeysReact.config({
+            left: () => {
+                this.props.move(-1, 0)
+            },
+            right: () => {
+                this.props.move(1, 0)
+            },
+            up: () => {
+                this.props.move(0, -1)
+            },
+            down: () => {
+                this.props.move(0, 1)
+            }
+        });
+
+        const blocks = this.state.blocks
+        const map = (<div tabIndex="1" style={style(blocks)}>
+            {around.map((row, x) =>  
+            row.map((tile, y) => (
+                <Tile {...tile} pos={[x,y]} key={x * 1024 + y} important={x === blocks && y === blocks}>
+                {
+                    (x === blocks && y === blocks) ? <img className="idle" /> : null
+                }
+                </Tile>
+            ))
+            )}
+        </div>)
+        
+        return (<div style={flexbox} {...ArrowKeysReact.events}>
+            {map}
+            <Controls move={this.props.move} />
+        </div>)
+    }
 }
 
 export default Map
