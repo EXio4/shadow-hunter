cfffa8666a37bb5c0ad499ab83f320e3455ef74f
Esteban I. RM <me@exio4.xyz>
2018-07-26 04:06:38 -0300

    rotate controls

--- src/components/Controls.js
+++ src/components/Controls.js
@@ -19,6 +19,7 @@ let style = {
     'right': '2vmin',
     'width': '32vmin',
     'height': '32vmin',
+    'transform': 'rotate(45deg)',
 }
 
 let button = {
@@ -52,10 +53,10 @@ const Controls = (props: Props) => {
     let e_left  = () => props.move(-1,  0)
     let e_right = () => props.move( 1,  0)
     return (<div style={style}>
-        <img alt="UP" style={up}    onClick={e_up}    onTouchdown={e_up}    src={i_up}   />
-        <img alt="DW" style={down}  onClick={e_down}  onTouchdown={e_down}  src={i_down} />
-        <img alt="LF" style={left}  onClick={e_left}  onTouchdown={e_left}  src={i_left} />
-        <img alt="RT" style={right} onClick={e_right} onTouchdown={e_right} src={i_right}/>
+        <img alt="UP" style={up}    onMousedown={e_up}    onTouchdown={e_up}    src={i_up}   />
+        <img alt="DW" style={down}  onMousedown={e_down}  onTouchdown={e_down}  src={i_down} />
+        <img alt="LF" style={left}  onMousedown={e_left}  onTouchdown={e_left}  src={i_left} />
+        <img alt="RT" style={right} onMousedown={e_right} onTouchdown={e_right} src={i_right}/>
      </div>
     )
 }
