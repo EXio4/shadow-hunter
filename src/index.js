ba57166b85a3d8376044ef828364d56bc87db725
Esteban I. RM <me@exio4.xyz>
2018-07-27 18:51:47 -0300

    eject + add version temporally

--- src/index.js
+++ src/index.js
@@ -4,5 +4,10 @@ import App from './App';
 import registerServiceWorker from './registerServiceWorker';
 import './style.css'
 
-ReactDOM.render(<App />, document.getElementById('root'));
+ReactDOM.render(<React.Fragment>
+    <App />
+    <div className="version">
+        {"shadowhunter-v0.1-dev"}
+    </div>
+    </React.Fragment>, document.getElementById('root'));
 registerServiceWorker();
