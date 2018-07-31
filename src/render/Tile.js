5a81e8cad7781fe90024bb4feeb42eefe4a49e95
Esteban I. Ruiz Moreno <exio4.com@gmail.com>
2018-07-31 19:45:58 -0300

    make height kinda works

--- src/render/Tile.js
+++ src/render/Tile.js
@@ -23,10 +23,10 @@ class Tile extends React.Component<ContextProps & TileProps> {
   constructor(props: ContextProps & TileProps) {
     super(props)
     this.ctx = props.ctx
-    this.geometry = new THREE.BoxGeometry(1, props.height, 1)
+    this.geometry = new THREE.PlaneGeometry(1, 1) // new THREE.BoxGeometry(1, props.height, 1)
     this.material = new THREE.MeshBasicMaterial( { color: props.color })
     this.cube = new THREE.Mesh(this.geometry, this.material)
-    this.cube.position.set(props.x, 0, props.y);
+    this.cube.position.set(props.x, props.height/12, props.y);
   }
 
   componentDidMount() {
@@ -39,14 +39,15 @@ class Tile extends React.Component<ContextProps & TileProps> {
     const newProps = this.props
     if (oldProps.height !== newProps.height) { 
       //this.geometry = new THREE.BoxGeometry(1, newProps.height, 1)
-      this.geometry = new THREE.PlaneGeometry(1, 1)
-      this.cube.geometry = this.geometry
+      //      this.geometry = new THREE.PlaneGeometry(1, 1)
+      this.cube.position.setY(newProps.height)
+     // this.cube.geometry = this.geometry
     }
     if (oldProps.color !== newProps.color) {
       this.material.color.set(newProps.color)
     }
     if (oldProps.x !== newProps.x || oldProps.y !== newProps.y) {
-      this.cube.position.set(newProps.x, 0, newProps.y);
+      this.cube.position.set(newProps.x, this.cube.position.y, newProps.y);
     }
   }
 
